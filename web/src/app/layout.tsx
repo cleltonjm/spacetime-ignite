3526b242ef6382328cf1e791a6d422c7f43c67fa
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-05-17 22:01:19 -0300

    Add files via upload

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjureeboto = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description: 'Uma cápsula do tempo construida com React, Next.js, TailwindCSS, e Typescript.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${baiJamjureeboto.variable} font-sans bg-gray-900 text-gray-100`}>
+        {children}
+      </body>
+    </html>
+  )
+}
